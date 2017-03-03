df137c3258bb477dae03ee9179794c0f914e960f
Sybren A. Stüvel <sybren@stuvel.eu>
2017-03-03 17:00:02 +0100

    Re-enabled Attract, it seems to work on Py3.6

--- cloud.py
+++ cloud.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 from pillar import PillarServer
-# from attract import AttractExtension
+from attract import AttractExtension
 # from flamenco import FlamencoExtension
 
-# attract = AttractExtension()
+attract = AttractExtension()
 # flamenco = FlamencoExtension()
 
 app = PillarServer('.')
-# app.load_extension(attract, '/attract')
+app.load_extension(attract, '/attract')
 # app.load_extension(flamenco, '/flamenco')
 app.process_extensions()
 
