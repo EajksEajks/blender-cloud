9da841efc3b2db895b18c6b1ec047a906c73a413
Sybren A. Stüvel <sybren@stuvel.eu>
2017-06-02 10:54:46 +0200

    Added Celery worker docker container
    This docker container uses the Blender Cloud image, but a different entry
    point. It is not intended to be network-reachable from the outside world.
    All it needs are connections to the databases (mongo, redis, rabbit).

---
+++ docker/4_run/celery-worker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source /install_scripts.sh
+source /manage.sh operations worker -- -C
--- docker/4_run/docker-entrypoint.sh
+++ docker/4_run/docker-entrypoint.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -f /installed ]; then
-    SITEPKG=$(echo /opt/python/lib/python3.*/site-packages)
-    echo "Installing Blender Cloud packages into $SITEPKG"
-
-    # TODO: 'pip3 install -e' runs 'setup.py develop', which runs 'setup.py egg_info',
-    # which can't write the egg info to the read-only /data/git volume. This is why
-    # we manually install the links.
-    for SUBPROJ in /data/git/{pillar,pillar-python-sdk,attract,flamenco}; do
-        NAME=$(python3 $SUBPROJ/setup.py --name)
-        echo "... $NAME"
-        echo $SUBPROJ >> $SITEPKG/easy-install.pth
-        echo $SUBPROJ > $SITEPKG/$NAME.egg-link
-    done
-    echo "All packages installed."
-
-    touch /installed
-fi
+source /install_scripts.sh
 
 # Make sure that log rotation works.
 mkdir -p ${APACHE_LOG_DIR}
