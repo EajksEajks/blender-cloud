9a692d475bb8638018545e8557267cc9486a7dcb
Sybren A. Stüvel <sybren@stuvel.eu>
2017-09-15 17:46:20 +0200

    Cache the entire homepage for 5 minutes.
    This means that comments and blog posts will take at most 5 minutes to show
    up on /.

--- cloud/routes.py
+++ cloud/routes.py
@@ -28,6 +28,10 @@ def homepage():
     if current_user.is_anonymous:
         return redirect(url_for('cloud.welcome'))
 
+    return _render_homepage()
+
+
+def _render_homepage():
     # Get latest blog posts
     api = system_util.pillar_api()
     latest_posts = Node.all({
@@ -319,3 +323,10 @@ def billing():
     return render_template(
         'users/settings/billing.html',
         store_user=store_user, groups=groups, title='billing')
+
+
+def setup_app(app):
+    global _render_homepage
+
+    cache = app.cache.cached(300)
+    _render_homepage = cache(_render_homepage)
