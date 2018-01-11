37b84cf75a24065201a4f523422539e52f43609a
Sybren A. Stüvel <sybren@stuvel.eu>
2018-01-11 11:20:18 +0100

    Upgraded ElasticSearch and Kibana to 6.1.1
    Requires a reset + reindex of everything (well, that's the easiest way to
    get things indexed properly again), which will loose us the Cloud stats.
    Before doing this, export those to MongDB and upgrade the statscollector
    to the version that I'll be committing soon.

--- docker/elastic/build.sh
+++ docker/elastic/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e
 
-docker build -t armadillica/elasticsearch:latest -f Dockerfile-elastic .
-docker build -t armadillica/kibana:latest -f Dockerfile-kibana .
+# When updating this, also update the versions in Dockerfile-*, and make sure that
+# it matches the versions of the elasticsearch and elasticsearch_dsl packages
+# used in Pillar. Those don't have to match exactly, but the major version should.
+VERSION=6.1.1
 
-echo "Done, built armadillica/elasticsearch:latest and armadillica/kibana:latest"
+docker build -t armadillica/elasticsearch:${VERSION} -f Dockerfile-elastic .
+docker build -t armadillica/kibana:${VERSION} -f Dockerfile-kibana .
+
+docker tag armadillica/elasticsearch:${VERSION} armadillica/elasticsearch:latest
+docker tag armadillica/kibana:${VERSION} armadillica/kibana:latest
+
+echo "Done, built armadillica/elasticsearch:${VERSION} and armadillica/kibana:${VERSION}"
+echo "Also tagged as armadillica/elasticsearch:latest and armadillica/kibana:latest"
