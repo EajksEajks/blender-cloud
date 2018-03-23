5a42e2dcb8a27bc2c634c74b3d863f85a2a73ccb
Sybren A. Stüvel <sybren@stuvel.eu>
2018-03-23 17:24:23 +0100

    Flip condition to unindent pretty much all the code
    No semantic changes.

--- docker/4_run/install_scripts.sh
+++ docker/4_run/install_scripts.sh
@@ -1,18 +1,20 @@
 
-if [ ! -f /installed ]; then
-    SITEPKG=$(echo /opt/python/lib/python3.*/site-packages)
-    echo "Installing Blender Cloud packages into $SITEPKG"
-
-    # TODO: 'pip3 install -e' runs 'setup.py develop', which runs 'setup.py egg_info',
-    # which can't write the egg info to the read-only /data/git volume. This is why
-    # we manually install the links.
-    for SUBPROJ in /data/git/{pillar,pillar-python-sdk,attract,flamenco,pillar-svnman}; do
-        NAME=$(python3 $SUBPROJ/setup.py --name)
-        echo "... $NAME"
-        echo $SUBPROJ >> $SITEPKG/easy-install.pth
-        echo $SUBPROJ > $SITEPKG/$NAME.egg-link
-    done
-    echo "All packages installed."
-
-    touch /installed
+if [ -f /installed ]; then
+    return
 fi
+
+SITEPKG=$(echo /opt/python/lib/python3.*/site-packages)
+echo "Installing Blender Cloud packages into $SITEPKG"
+
+# TODO: 'pip3 install -e' runs 'setup.py develop', which runs 'setup.py egg_info',
+# which can't write the egg info to the read-only /data/git volume. This is why
+# we manually install the links.
+for SUBPROJ in /data/git/{pillar,pillar-python-sdk,attract,flamenco,pillar-svnman}; do
+    NAME=$(python3 $SUBPROJ/setup.py --name)
+    echo "... $NAME"
+    echo $SUBPROJ >> $SITEPKG/easy-install.pth
+    echo $SUBPROJ > $SITEPKG/$NAME.egg-link
+done
+echo "All packages installed."
+
+touch /installed
